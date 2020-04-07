05cac0695109a1b23d7b6fd34e6a2cf4e5aa1d55
Vinoth Kumar Mohanraj <vinoth.july@gmail.com>
2020-04-07 14:19:23 +0530

    Added IOUState & Flow Changes

--- contracts/src/main/kotlin/com/template/states/IOUState.kt
+++ contracts/src/main/kotlin/com/template/states/IOUState.kt
@@ -4,9 +4,12 @@ import com.template.contracts.TemplateContract
 import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
 
 // *********
 // * State *
 // *********
 @BelongsToContract(TemplateContract::class)
-data class TemplateState(val data: String, override val participants: List<AbstractParty> = listOf()) : ContractState
+data class IOUState(val value: Int, val lender: Party, val borrower: Party) : ContractState {
+    override val participants get() = listOf(lender, borrower)
+}
--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -1,7 +1,12 @@
 package com.template.flows
 
 import co.paralleluniverse.fibers.Suspendable
+import com.template.contracts.TemplateContract
+import com.template.states.IOUState
+import net.corda.core.contracts.Command
 import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 
 // *********
@@ -9,19 +14,34 @@ import net.corda.core.utilities.ProgressTracker
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
+class IOUFlow(val iouValue: Int, val otherParty: Party) : FlowLogic<Unit>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
     override fun call() {
         // Initiator flow logic goes here.
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+
+        val outputState = IOUState(100, ourIdentity, otherParty)
+        val command = Command(TemplateContract.Commands.Action(), ourIdentity.owningKey)
+
+        val txBuilder = TransactionBuilder(notary = notary)
+                .addOutputState(outputState, TemplateContract.ID)
+                .addCommand(command)
+
+        val signedTransaction = serviceHub.signInitialTransaction(txBuilder)
+
+        val otherPartySession = initiateFlow(otherParty)
+
+        subFlow(FinalityFlow(signedTransaction, otherPartySession))
     }
 }
 
-@InitiatedBy(Initiator::class)
-class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+@InitiatedBy(IOUFlow::class)
+class IOUFlowResponder(val counterPartySession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
         // Responder flow logic goes here.
+        subFlow(ReceiveFinalityFlow(counterPartySession))
     }
 }
