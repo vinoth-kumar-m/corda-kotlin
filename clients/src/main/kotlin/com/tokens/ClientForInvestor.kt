9dfc1aed70fc974572de3b0cdccb2f4c129096a6
vinoth-kumar-m <vinoth.july@gmail.com>
2020-04-10 12:39:18 +0530

    Binding to 0.0.0.0 instead of localhost

--- clients/src/main/kotlin/com/tokens/ClientForInvestor.kt
+++ clients/src/main/kotlin/com/tokens/ClientForInvestor.kt
@@ -5,6 +5,7 @@ import com.r3.corda.lib.accounts.workflows.flows.CreateAccount
 import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.contracts.StateAndRef
 import net.corda.core.messaging.startFlow
+import net.corda.core.utilities.NetworkHostAndPort
 import net.corda.core.utilities.NetworkHostAndPort.Companion.parse
 import net.corda.core.utilities.getOrThrow
 import net.corda.core.utilities.loggerFor
@@ -23,11 +24,12 @@ private class ClientForInvestor {
 
     fun main(args: Array<String>) {
         // Create an RPC connection to the node.
-        require(args.size == 3) { "Usage: Client <node address> <rpc username> <rpc password>" }
-        val nodeAddress = parse(args[0])
-        val rpcUsername = args[1]
-        val rpcPassword = args[2]
-        val client = CordaRPCClient(nodeAddress)
+        require(args.size == 4) { "Usage: Client <node address> <port> <rpc username> <rpc password>" }
+        val nodeAddress = args[0]
+        val port: Int = args[1].toInt()
+        val rpcUsername = args[2]
+        val rpcPassword = args[3]
+        val client = CordaRPCClient(NetworkHostAndPort(nodeAddress, port))
         val rpcOps = client.start(rpcUsername, rpcPassword).proxy
 
         val account: StateAndRef<AccountInfo> = rpcOps.startFlow(::CreateAccount, "vinoth-kumar-m").returnValue.getOrThrow()
