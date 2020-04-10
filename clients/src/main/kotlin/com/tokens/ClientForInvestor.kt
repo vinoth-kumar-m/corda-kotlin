76f07e23bd78d0fc6a054e0ee1d33130afd85d8c
vinoth-kumar-m <vinoth.july@gmail.com>
2020-04-10 12:06:19 +0530

    Added Client side logic

--- clients/src/main/kotlin/com/tokens/ClientForInvestor.kt
+++ clients/src/main/kotlin/com/tokens/ClientForInvestor.kt
@@ -1,7 +1,12 @@
 package com.tokens
 
+import com.r3.corda.lib.accounts.contracts.states.AccountInfo
+import com.r3.corda.lib.accounts.workflows.flows.CreateAccount
 import net.corda.client.rpc.CordaRPCClient
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.messaging.startFlow
 import net.corda.core.utilities.NetworkHostAndPort.Companion.parse
+import net.corda.core.utilities.getOrThrow
 import net.corda.core.utilities.loggerFor
 
 /**
@@ -9,11 +14,11 @@ import net.corda.core.utilities.loggerFor
  *
  * The RPC connection is configured using command line arguments.
  */
-fun main(args: Array<String>) = Client().main(args)
+fun main(args: Array<String>) = ClientForInvestor().main(args)
 
-private class Client {
+private class ClientForInvestor {
     companion object {
-        val logger = loggerFor<Client>()
+        val logger = loggerFor<ClientForInvestor>()
     }
 
     fun main(args: Array<String>) {
@@ -23,11 +28,10 @@ private class Client {
         val rpcUsername = args[1]
         val rpcPassword = args[2]
         val client = CordaRPCClient(nodeAddress)
-        val proxy = client.start(rpcUsername, rpcPassword).proxy
+        val rpcOps = client.start(rpcUsername, rpcPassword).proxy
 
-        // Interact with the node.
-        // For example, here we print the nodes on the network.
-        val nodes = proxy.networkMapSnapshot()
-        logger.info("{}", nodes)
+        val account: StateAndRef<AccountInfo> = rpcOps.startFlow(::CreateAccount, "vinoth-kumar-m").returnValue.getOrThrow()
+
+        logger.info("Account created successfully.")
     }
 }
