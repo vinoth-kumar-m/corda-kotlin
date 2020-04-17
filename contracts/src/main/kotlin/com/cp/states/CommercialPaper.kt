2feffa0806934c7f7ef409eb1a811197aabc91d9
vinoth-kumar-m <vinoth.july@gmail.com>
2020-04-17 08:55:42 +0530

    Added State grouping method

--- contracts/src/main/kotlin/com/cp/states/CommercialPaper.kt
+++ contracts/src/main/kotlin/com/cp/states/CommercialPaper.kt
@@ -5,7 +5,9 @@ import net.corda.core.contracts.Amount
 import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.crypto.NullKeys
 import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.AnonymousParty
 import net.corda.core.identity.Party
 import java.time.Instant
 import java.util.*
@@ -24,6 +26,8 @@ data class CommercialPaper(val faceValue: Amount<Currency>,
                            override val linearId: UniqueIdentifier = UniqueIdentifier()) : LinearState {
 
     override val participants: List<AbstractParty> get() = listOf(issuer, owner)
+
+    fun withoutOwner() = copy(account = AnonymousParty(owningKey = NullKeys.NullPublicKey))
 
     fun withNewOwner(newAccount: AbstractParty, newOwner: Party) = copy(account = newAccount, owner = newOwner)
 
