83c542e201bbdc78ed98be3d920d7241109d74cd
vinoth-kumar-m <vinoth.july@gmail.com>
2020-04-20 12:21:51 +0530

    Client code added

--- clients/src/main/kotlin/com/cp/webserver/Controller.kt
+++ clients/src/main/kotlin/com/cp/webserver/Controller.kt
@@ -28,15 +28,70 @@ class Controller(private val service: Service) {
             val accountInfo = service.createAccount(name)
             logger.info("Account successfully created.")
             return ResponseEntity.ok(accountInfo)
-        } catch(ex: Exception) {
+        } catch (ex: Exception) {
             logger.error("Exception occurred while creating account: ${ex.message}")
         }
         return ResponseEntity(null, HttpStatus.BAD_REQUEST)
     }
 
-    @GetMapping(value = ["/issue"], produces = ["text/plain"])
+    @GetMapping(value = ["/accounts"], produces = ["application/json"])
+    private fun allAccounts(): ResponseEntity<List<AccountInfo>> {
+        logger.info("Retrieving all accounts")
+        try {
+            val accounts = service.allAccounts()
+            return ResponseEntity.ok(accounts)
+        } catch (ex: Exception) {
+            logger.error("Exception occurred while retrieving all accounts: ${ex.message}")
+        }
+        return ResponseEntity(null, HttpStatus.BAD_REQUEST)
+    }
+
+    @GetMapping(value = ["/issue"], produces = ["application/json"])
     private fun issue(@RequestParam identifier: String, @RequestParam faceValue: Int): ResponseEntity<CommercialPaper> {
-        logger.info("Identified: $identifier, Face Value: $faceValue")
+        logger.info("Identifier: $identifier, Face Value: $faceValue")
+        try {
+            val commercialPaper = service.issueCommercialPaper(identifier, faceValue)
+            return ResponseEntity.ok(commercialPaper)
+        } catch (ex: Exception) {
+            logger.error("Exception occurred while issuing commercial paper: ${ex.message}")
+        }
         return ResponseEntity(HttpStatus.OK)
     }
+
+    @GetMapping(value = ["/retrieve"], produces = ["application/json"])
+    private fun retrieve(@RequestParam name: String): ResponseEntity<List<CommercialPaper>> {
+        logger.info("Name: $name")
+        try {
+            val commercialPapers = service.retrieveCommercialPapersByName(name)
+            return ResponseEntity.ok(commercialPapers)
+        } catch (ex: Exception) {
+            logger.error("Exception occurred while retrieving commercial papers: ${ex.message}")
+        }
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/transfer"], produces = ["application/json"])
+    private fun transfer(@RequestParam identifier: String, @RequestParam fromAccount: String, @RequestParam toAccount: String): ResponseEntity<CommercialPaper> {
+        logger.info("Transfer $identifier from $fromAccount to $toAccount")
+        try {
+            val commercialPaper = service.transferCommercialPaper(identifier, fromAccount, toAccount)
+            return ResponseEntity.ok(commercialPaper)
+        } catch (ex: Exception) {
+            logger.error("Exception occurred while transferring commercial paper: ${ex.message}")
+        }
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/redeem"], produces = ["application/json"])
+    private fun redeem(@RequestParam identifier: String, @RequestParam fromAccount: String): ResponseEntity<CommercialPaper> {
+        logger.info("Redeem $identifier from $fromAccount")
+        try {
+            val commercialPaper = service.redeemCommercialPaper(identifier, fromAccount)
+            return ResponseEntity.ok(commercialPaper)
+        } catch (ex: Exception) {
+            logger.error("Exception occurred while redeeming commercial paper: ${ex.message}")
+        }
+        return ResponseEntity(HttpStatus.OK)
+    }
+
 }
--- clients/src/main/kotlin/com/cp/webserver/Service.kt
+++ clients/src/main/kotlin/com/cp/webserver/Service.kt
@@ -1,13 +1,23 @@
 package com.cp.webserver
 
+import com.cp.flows.IssueCommercialPaperFlow
+import com.cp.flows.QueryCommercialPaperByAccountName
+import com.cp.flows.RedeemCommercialPaperFlow
+import com.cp.flows.TransferCommercialPaperFlow
+import com.cp.states.CommercialPaper
 import com.r3.corda.lib.accounts.contracts.states.AccountInfo
+import com.r3.corda.lib.accounts.workflows.flows.AllAccounts
 import com.r3.corda.lib.accounts.workflows.flows.CreateAccount
 import com.r3.corda.lib.accounts.workflows.flows.ShareAccountInfo
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.messaging.startFlow
 import net.corda.core.utilities.getOrThrow
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
+import java.util.*
 
 /**
  * Define your API endpoints here.
@@ -22,10 +32,98 @@ class Service(rpc: NodeRPCConnection) {
     private val rpcOps = rpc.proxy
 
     fun createAccount(name: String): AccountInfo {
-        val accountInfo = rpcOps.startFlow(::CreateAccount, name).returnValue.getOrThrow()
+        val accountStateAndRef = rpcOps.startFlow(::CreateAccount, name).returnValue.getOrThrow()
         val issuer = rpcOps.wellKnownPartyFromX500Name(CordaX500Name("Issuer", "London", "GB"))
                 ?: throw Exception("Issuer information not available")
-        rpcOps.startFlow(::ShareAccountInfo, accountInfo, listOf(issuer))
-        return accountInfo.state.data
+        rpcOps.startFlow(::ShareAccountInfo, accountStateAndRef, listOf(issuer))
+        val accountInfo = accountStateAndRef.state.data
+        logger.info("Account Info: {}", accountInfo)
+        return accountInfo
+    }
+
+    fun allAccounts(): List<AccountInfo> {
+        val accounts = rpcOps.startFlow(::AllAccounts).returnValue.getOrThrow()
+        logger.info("Total Accounts: {}", accounts.size)
+        return accounts.map { it -> it.state.data }
+    }
+
+    fun issueCommercialPaper(identifier: String, faceValue: Int): CommercialPaper{
+        val identifier = UUID.fromString(identifier)
+                ?: throw Exception("Couldn't generate UUID from String")
+        logger.debug("Identifier: {}", identifier)
+
+        val investor = rpcOps.wellKnownPartyFromX500Name(CordaX500Name("Investor", "New York", "US"))
+                ?: throw Exception("Investor information not available")
+        logger.debug("Investor: {}", investor)
+
+        logger.info("Issuing commercial paper..")
+        val signedTransaction = rpcOps.startFlow(
+                ::IssueCommercialPaperFlow,
+                Amount.fromDecimal(BigDecimal(faceValue), Currency.getInstance(Locale.US)),
+                identifier,
+                investor).returnValue.getOrThrow()
+
+        val commercialPaper = signedTransaction.tx.outputs.single().data as CommercialPaper
+        logger.info("Commercial Paper Issued with Identifier: {}", commercialPaper)
+
+        return commercialPaper
+    }
+
+    fun retrieveCommercialPapersByName(name: String): List<CommercialPaper> {
+        logger.info("Retrieving Commercial Papers By AccountName: $name")
+        val commercialPapers = rpcOps.startFlow(::QueryCommercialPaperByAccountName, name).returnValue.getOrThrow()
+        logger.info("Total Commercial Papers for $name: {}", commercialPapers.size)
+        return commercialPapers
+    }
+
+    fun transferCommercialPaper(identifier: String, fromAccount: String, toAccount: String): CommercialPaper {
+        val fromAccountIdentifier = UUID.fromString(fromAccount)
+                ?: throw Exception("Couldn't generate UUID from String")
+
+        val toAccountIdentifier = UUID.fromString(toAccount)
+                ?: throw Exception("Couldn't generate UUID from String")
+
+        logger.debug("From Account: {}, To Account: {}", fromAccount, toAccount)
+
+        val linearId = UniqueIdentifier(externalId = null, id = UUID.fromString(identifier))
+        logger.info("Transfer Commercial Paper: {}", linearId)
+
+        val investor = rpcOps.wellKnownPartyFromX500Name(CordaX500Name("Investor", "New York", "US"))
+                ?: throw Exception("Investor information not available")
+        logger.debug("Investor: {}", investor)
+
+        val signedTransaction = rpcOps.startFlow(
+                ::TransferCommercialPaperFlow,
+                linearId,
+                fromAccountIdentifier,
+                toAccountIdentifier,
+                investor).returnValue.getOrThrow()
+
+        val commercialPaper = signedTransaction.tx.outputStates.single() as CommercialPaper
+
+        logger.info("Commercial Paper transferred with Identifier: {}", commercialPaper)
+
+        return commercialPaper
+    }
+
+    fun redeemCommercialPaper(identifier: String, fromAccount: String): CommercialPaper {
+        val fromAccountIdentifier = UUID.fromString(fromAccount)
+                ?: throw Exception("Couldn't generate UUID from String")
+
+        logger.debug("From Account: {}", fromAccount)
+
+        val linearId = UniqueIdentifier(externalId = null, id = UUID.fromString(identifier))
+        logger.info("Redeem Commercial Paper: {}", linearId)
+
+        val signedTransaction = rpcOps.startFlow(
+                ::RedeemCommercialPaperFlow,
+                linearId,
+                fromAccountIdentifier).returnValue.getOrThrow()
+
+        val commercialPaper = signedTransaction.tx.outputStates.single() as CommercialPaper
+
+        logger.info("Commercial Paper redeemed with Identifier: {}", commercialPaper)
+
+        return commercialPaper
     }
 }
